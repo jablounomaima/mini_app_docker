9c6e5fa326360fff4d640989ca3da5c893c76182
omaimajabloun <jabloun.omaima5102000@gmail.com>
2025-04-14 10:52:39 +0200

    Ajout d'un front-end simple

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Bonjour! Cette application Python fonctionne dans Docker!"
-
+ return render_template('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
