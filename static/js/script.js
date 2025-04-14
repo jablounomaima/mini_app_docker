de47f726a15396b9ae700bba2cfb77673ffceb14
omaimajabloun <jabloun.omaima5102000@gmail.com>
2025-04-14 11:31:02 +0200

    Ajout d'un front-end HTML/CSS/JS et amélioration de l'application

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// static/js/script.js
+document.getElementById('click-me').addEventListener('click', () => {
+    document.getElementById('message').textContent = 'Bonjour depuis le front-end !';
+});
